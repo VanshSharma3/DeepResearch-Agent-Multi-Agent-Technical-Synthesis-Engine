5dc0c412922c9e1ed95f82f6bb327e7960e8e775
Vansh Sharma <vansh.sharma7171717@gmail.com>
2026-08-14 17:37:13 +0530

    Update CORS origins

--- server.py
+++ server.py
@@ -19,10 +19,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-    ],
+    allow_origins=["*"],  # Allows local Live Server + any deployed frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
